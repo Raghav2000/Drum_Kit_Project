df38d5f771ebc5ded6fc1c3c9da56a2d3914cdce
Raghav2000 <raghav292000@gmail.com>
2022-10-10 22:32:20 +0530

    adding animation to active buttons

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i=0;i<numberOfDrumButtons;i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-
     var buttonInnerHTML = this.innerHTML
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 document.addEventListener("keydown",function (event){
   makeSound(event.key);
+  buttonAnimation(event.key);
 })
 
 function makeSound(key){
@@ -49,4 +50,13 @@ function makeSound(key){
       audio.play();
       break;
   }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
